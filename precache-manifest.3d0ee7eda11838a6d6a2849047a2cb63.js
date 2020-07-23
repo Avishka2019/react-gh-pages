84582a91f442ddc315a6554e17895bd9f07b815f
Avishka2019 <avishkadevinda2017@gmail.com>
2020-07-23 11:41:37 +0530

    Updates

---
+++ precache-manifest.3d0ee7eda11838a6d6a2849047a2cb63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "002576381f91703a6e2f",
+    "url": "/react-gh-pages/static/css/main.766cadc1.chunk.css"
+  },
+  {
+    "revision": "002576381f91703a6e2f",
+    "url": "/react-gh-pages/static/js/main.00257638.chunk.js"
+  },
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/react-gh-pages/static/js/2.3791395a.chunk.js"
+  },
+  {
+    "revision": "4b2c5203b245217a3678d0620a088b6f",
+    "url": "/react-gh-pages/index.html"
+  }
+];
